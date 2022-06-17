330ab042d34b1c7f269958bc0bc0b54e7e299d7a
NgoManh131 <ngotienmanh131@gmail.com>
2022-06-17 14:12:25 +0700

    javascript

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+
