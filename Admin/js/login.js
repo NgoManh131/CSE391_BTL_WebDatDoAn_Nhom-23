964d9ccaf715098ca4f28055b6e9d418feea11c4
NgoManh131 <ngotienmanh131@gmail.com>
2022-06-23 09:21:47 +0700

    admin

--- Admin/js/login.js
+++ Admin/js/login.js
@@ -1,5 +1,3 @@
-/* =========================================== */
-/* =========================================== */
 function validate() {
      var username = document.getElementById("username").value;
      var password = document.getElementById("password-field").value;
